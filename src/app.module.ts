7b123a4e4e833fc8cbd631f12101468245844318
developer-jessmeshee <developer.jessmeshee@gmail.com>
2026-03-02 21:51:26 -0600

    Se realizaron las configuraciones necesarias para conectarse a la base de datos de Mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [
+        ConfigModule
+      ],
+      useFactory: async ( config: ConfigService ) => ({
+        uri: config.get<string>( 'MONGO_URI' ),
+        maxPoolSize: parseInt( config.get( 'MONGO_MAX_POOL', '100' ) ),
+        minPoolSize: parseInt( config.get( 'MONGO_MIN_POOL', '0' ) ),
+        serverSelectionTimeoutMS: 5000,
+        socketTimeoutMS: 45000,
+        autoIndex: config.get('NODE_ENV') !== 'production',
+      }),
+      inject: [
+        ConfigService
+      ],
+    }),
+  ],
   controllers: [
     AppController,
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dns from 'node:dns';
+
+dns.setServers([ '1.1.1.1', '8.8.8.8' ]);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
